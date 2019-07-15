44d6cc222391b8d11946aa836dffa2ee69d6b4b2
Akim Essen <akimess@gmail.com>
2019-07-15 14:59:31 +0300

    Schedule Billing Service

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -5,14 +5,32 @@ import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.core.exceptions.NetworkException
 import mu.KotlinLogging
+import java.time.LocalDateTime
 import java.util.*
 import kotlin.concurrent.schedule
+import kotlin.concurrent.scheduleAtFixedRate
 
 class BillingService(
     private val paymentProvider: PaymentProvider,
     private val invoiceService: InvoiceService
 ) {
     private val logger = KotlinLogging.logger {}
+
+
+    fun start(): TimerTask {
+        return Timer("processAllInvoices", false)
+                .scheduleAtFixedRate(
+                        //Start after 1 hour
+                        delay = 36000,
+                        //Repeat after 24 hours
+                        period = 36000 * 24
+                ) {
+                    //Start the process if it's the first day of the month
+                    if (LocalDateTime.now().dayOfMonth == 1){
+                        processAllInvoices()
+                    }
+                }
+    }
 
     //Single Invoice Process
     fun processInvoice(invoice: Invoice): Invoice {
