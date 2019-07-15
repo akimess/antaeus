83341ac90c4084f4fead78d266b720703cda2d8e
Akim Essen <akimess@gmail.com>
2019-07-15 13:17:31 +0300

    Added test for invoice status update

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -4,12 +4,21 @@ import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.Money
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.InvoiceStatus
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class InvoiceServiceTest {
+    private val invoiceObj = Invoice(200, 1, Money(200.toBigDecimal(),
+            Currency.EUR), InvoiceStatus.PENDING)
+    private val paidInvoiceObj = invoiceObj.copy(status = InvoiceStatus.PAID)
     private val dal = mockk<AntaeusDal> {
         every { fetchInvoice(404) } returns null
+        every { fetchInvoice(200) } returns invoiceObj
+        every { updateInvoice(invoiceObj) } returns paidInvoiceObj
     }
 
     private val invoiceService = InvoiceService(dal = dal)
@@ -20,4 +29,12 @@ class InvoiceServiceTest {
             invoiceService.fetch(404)
         }
     }
+
+    @Test
+    fun `will change status to PAID`() {
+        assert(invoiceService.changeStatus(invoiceObj, InvoiceStatus.PAID) ==
+                paidInvoiceObj )
+    }
+
+
 }
